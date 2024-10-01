42e24ae68c5f0e0d06f3588ac2dceabfe8b94ca8
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2024-10-01 18:16:24 +0530

    feat: testing

--- main.sh
+++ main.sh
@@ -112,7 +112,7 @@ JSON_PAYLOAD+=$(cat <<EOF
         },
         {
           "title": "Actions URL",
-          "value": "<$REPO_ACTION_URL|clouddrove>",
+          "value": "<$REPO_ACTION_URL|Slack_workflow>",
           "short": true
         },
         {
