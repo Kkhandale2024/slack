01f3f976e822833c2112dde80a647fb47b1be544
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2024-10-01 14:18:47 +0530

    feat: testing

--- main.sh
+++ main.sh
@@ -42,6 +42,7 @@ echo "DEBUG: MESSAGE is: '$MESSAGE'"
 # Example environment variables for demo purposes
 REF="${GITHUB_REF:-'N/A'}"
 EVENT="${GITHUB_EVENT_NAME:-'N/A'}"
+COMMIT_ID="${GITHUB_SHA:-'N/A'}"  # Get the commit ID (SHA)
 
 # Define a map of predefined colors for various job statuses
 declare -A COLORS
@@ -106,6 +107,11 @@ JSON_PAYLOAD+=$(cat <<EOF
         {
           "title": "Event",
           "value": "$EVENT",
+          "short": true
+        },
+        {
+          "title": "Commit ID",
+          "value": "$COMMIT_ID",
           "short": true
         },
 EOF
@@ -263,4 +269,3 @@ echo "DEBUG: JSON_PAYLOAD is: $JSON_PAYLOAD"
 curl -X POST -H "Content-Type: application/json" -d "$JSON_PAYLOAD" "$SLACK_WEBHOOK_URL"
 
 echo "Successfully sent the message to Slack!"
-
