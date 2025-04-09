a468ed4acfee3c2f25e856fa4d5f65dd9c14039f
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2025-04-09 15:30:25 +0530

    feat: testing terraform

---
+++ infra/providers.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = "my-project-44865-424207"
+  region  = "us-east1"
+}
+
+# terraform {
+#   backend "gcs" {
+#     bucket = "kontrol-dev-terraform-backend"
+#     prefix = "terraform/dev"
+#   }
+#   required_providers {
+#     kubectl = {
+#       source  = "gavinbunney/kubectl"
+#       version = ">= 1.7.0"
+#     }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = ">= 2.0.0"
+#     }
+#   }
+# }
